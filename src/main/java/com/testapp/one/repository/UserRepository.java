04a9d654521ea9f8f3027de5616eeb2664f9418b
Aly Nielson <nielsonaly@gmail.com>
2019-01-23 13:19:13 -0600

    create user repository

---
+++ src/main/java/com/testapp/one/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.testapp.one.repository;
+
+import com.testapp.one.domain.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+}
