3c19e1c1b610db9599f9215d0c7b42f998e707cc
Aly Nielson <nielsonaly@gmail.com>
2019-01-23 14:56:00 -0600

    user controller create new request

--- src/main/java/com/testapp/one/rest/v1/controller/UserController.java
+++ src/main/java/com/testapp/one/rest/v1/controller/UserController.java
@@ -1,11 +1,44 @@
 package com.testapp.one.rest.v1.controller;
 
+import com.testapp.one.rest.v1.dto.UserDto;
+import com.testapp.one.rest.v1.dto.mapper.UserDtoMapper;
+import com.testapp.one.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "api/v1/user", produces = "application/json")
 public class UserController {
+
+    private final UserDtoMapper userDtoMapper;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserDtoMapper userDtoMapper,
+                          UserService userService) {
+        this.userDtoMapper = userDtoMapper;
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto create(
+            @RequestBody
+            @Validated
+            UserDto userDto
+    ) {
+        return userDtoMapper.toDto(
+                userService.createNew(
+                        userDtoMapper.toDomain(userDto)
+                ));
+    }
+
 }
